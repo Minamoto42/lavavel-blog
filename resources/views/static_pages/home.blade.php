7aed7ae908ce1952a0d6568e51021eb33ab81fb7
Minamoto42 <ryugenrokuroku@gmail.com>
2025-02-17 18:56:03 +0900

    lavavel_practice_2.1

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -4,10 +4,10 @@
     <div class="bg-light p-3 p-sm-5 rounded">
         <h1>Minamoto</h1>
         <p class="lead">
-            Hi, I'm Minamoto.
+            I'm Minamoto.
         </p>
         <p>
-            This is the first web application I built using Laravel.
+            欢迎来到我的Mina妙妙屋.
         </p>
         <p>
             <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">Signup now</a>
